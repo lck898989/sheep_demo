6bc018272d49d6ea2f93e10eff6af544abd4fbab
lck898989 <1783887127@qq.com>
2022-10-02 23:07:42 +0800

    feat: 添加地图代码

--- assets/scripts/editor/map.ts
+++ assets/scripts/editor/map.ts
@@ -13,13 +13,16 @@ export class map extends Component {
 	@property(Node)
 	cards: Node = null;
 
-	private mapStart: Vec3 = new Vec3(-200, 300, 0);
+	private mapStart: Vec3 = new Vec3(-280, 300, 0);
 
-	private row: number = 6;
-	private col: number = 6;
+	private row: number = 14;
+	private col: number = 14;
 
-	private mapWidth = 80;
-	private mapHeight = 80;
+	private mapWidth = 40;
+	private mapHeight = 40;
+
+	private spaceX = 0;
+	private spaceY = 0;
 
 	private map: Vec3[][] = [];
 
@@ -34,36 +37,40 @@ export class map extends Component {
 
 	_initMap() {
 		console.log("init map");
-		const mapCenterStart: Vec3 = new Vec3(-160,260,0);
+		// const mapCenterStart: Vec3 = new Vec3(-280,260,0);
 		
 		for (let i = 0; i < this.row; i++) {
 			for(let j = 0; j < this.col; j++) {
 				const prefabNode = instantiate(this.mapItem);
-				const x = this.mapStart.x + j * this.mapWidth;
-				const y = this.mapStart.y - i * this.mapHeight;
+				const x = this.mapStart.x + j * this.mapWidth + this.spaceX * j;
+				const y = this.mapStart.y - i * this.mapHeight - this.spaceY * i;
 				this.node.addChild(prefabNode);
 				const pos: Vec3 = new Vec3(x,y,this.mapStart.z);
 
 				prefabNode.setPosition(pos);
 				this.mapItemMap[`${pos.x}${pos.y}`] = prefabNode;
-				
-				const card = instantiate(this.cartItem);
-				this.cards.addChild(card);
-				card.setPosition(pos);
-				
-				const cardComp: Card = card.getComponent(Card);
-				cardComp.setType(this.randomType[createRandom(0,7)]);
-				
-				if(i < this.row - 1 && j < this.col - 1) {
-					const centerMapItem = instantiate(this.mapItem);
-					const x = mapCenterStart.x + j * this.mapWidth;
-					const y = mapCenterStart.y - i * this.mapHeight;
-					this.node.addChild(centerMapItem);
-					const centPos = new Vec3(x,y,mapCenterStart.z);
-					centerMapItem.setPosition(new Vec3(x,y,mapCenterStart.z));
 
-					this.mapItemMap[`${centPos.x}${centPos.y}`] = centerMapItem; 
+				if(!i) {
+					const card = instantiate(this.cartItem);
+					this.cards.addChild(card);
+					card.setPosition(pos);
+					
+					const cardComp: Card = card.getComponent(Card);
+					cardComp.setType(this.randomType[createRandom(0,7)]);
 				}
+				
+				
+				
+				// if(i < this.row - 1 && j < this.col - 1) {
+				// 	const centerMapItem = instantiate(this.mapItem);
+				// 	const x = mapCenterStart.x + j * this.mapWidth;
+				// 	const y = mapCenterStart.y - i * this.mapHeight;
+				// 	this.node.addChild(centerMapItem);
+				// 	const centPos = new Vec3(x,y,mapCenterStart.z);
+				// 	centerMapItem.setPosition(new Vec3(x,y,mapCenterStart.z));
+
+				// 	this.mapItemMap[`${centPos.x}${centPos.y}`] = centerMapItem; 
+				// }
 			}
 
 		}
